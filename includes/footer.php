8853bec3d2a23a5a7872f2c6e6b2c6c4a9854de2
Ophimacus <118141803+Ophimacus@users.noreply.github.com>
2025-05-20 14:26:01 +0800

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                 Online Library Management System</a> 
+                 Online Library Management System 
                 </div>
 
             </div>
